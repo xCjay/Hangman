460efc8b053f5296180cf75bbcd16b649dc4d53d
Cjay <millercaleb425@gmail.com>
2022-02-24 13:44:12 -0700

    2/24/22
    added guessing

--- src/Hangman.java
+++ src/Hangman.java
@@ -3,9 +3,11 @@ import acm.graphics.GLine;
 import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
+import java.awt.event.ActionEvent;
 
 import svu.csc213.Dialog;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Hangman extends GraphicsProgram{
@@ -13,10 +15,15 @@ public class Hangman extends GraphicsProgram{
     Man man = new Man();
     Word word = new Word();
 
+    int life = 6;
+
     String usedWord = word.getWord();
 
     Letter letter = new Letter(usedWord, 0);
 
+    Letter[] letArray = new Letter[usedWord.length()];
+
+    public JButton guessLetterBtn;
 
 
     GRect dash;
@@ -27,6 +34,8 @@ public class Hangman extends GraphicsProgram{
     @Override
     public void run() {
         genWord();
+        genLett();
+        addActionListeners();
     }
 
     @Override
@@ -38,7 +47,48 @@ public class Hangman extends GraphicsProgram{
 
         add(letter, 500, 500);
         add(man, 100, 100);
+        guessLetterBtn = new JButton("GuessLetter");
+        add(guessLetterBtn, SOUTH);
 
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        switch (ae.getActionCommand()) {
+            case "GuessLetter":
+                guessLetter();
+                break;
+        }
+    }
+
+    private void guessLetter() {
+        String guess = Dialog.getString("Guess A Letter");
+
+        guessLet(guess.charAt(0));
+    }
+
+
+
+
+    private void genLett(){
+        for (int i = 0; i < usedWord.length(); i++) {
+
+            letArray[i] = new Letter(usedWord, i);
+
+            add(letArray[i], getWidth()/2+ 20*(i+2), getHeight()/2);
+        }
+    }
+
+    private void guessLet(char a){
+        for (int i = 0; i < usedWord.length(); i++) {
+            if (letArray[i].getLetter() == a){
+                letArray[i].setVis(true);
+            }
+        }
+    }
+
+    private void loseLife(){
 
     }
 
--- src/Letter.java
+++ src/Letter.java
@@ -2,19 +2,31 @@ import acm.graphics.GLabel;
 
 public class Letter extends GLabel {
 
+    private char letter;
 
     public Letter(String word, int char1){
         super(null);
 
+        letter = toArray(word, char1);
+
         setLabel(String.valueOf(toArray(word, char1)));
         setFont("Calibri-30");
-
+        setVisible(false);
 
     }
 
     public char toArray(String word, int char1){
         char[] char2 = word.toCharArray();
+
         return char2[char1];
+    }
+
+    public void setVis(boolean set){
+        setVisible(set);
+    }
+
+    public char getLetter(){
+        return letter;
     }
 
 
